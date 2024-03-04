b14d76262e07f8fca90eeb0142f6a96e271102e4
Luke Wheldale <lukewheldale@gmail.com>
2024-03-04 15:04:20 +1030

    operate function

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
 	return a / b;
 };
+
+// takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+let firstNum;
+let secondNum;
+let operator;
+const operate = (operator, firstNum, secondNum) => {
+	if (operator == "add") {
+		return add(firstNum, secondNum);
+	} else if (operator == "subtract") {
+		return subtract(firstNum, secondNum);
+	} else if (operator == "multiply") {
+		return multiply(firstNum, secondNum);
+	} else if (operator == "divide") {
+		return divide(firstNum, secondNum);
+	} else {
+		return null;
+	}
+};
+
+console.log(operate("divide", 10, 2));
